b5328fa470cc9e8d19a825a41e5eaedb5028f329
maxa2 <maxa@live.ca>
2023-03-26 20:37:09 -0700

    Added stuff

---
+++ admin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html class="center">
+    <head lang="en"> 
+    <meta charset="utf-8">
+    <link rel = "stylesheet" href = "CSS/reset.css">
+    <link rel = "stylesheet" href="CSS/homePageStyle.css">
+    <script> function noLog(){alert('No user detected, log in please');}</script>
+        <title>CRUELTY SQUAD FANPAGE</title>
+    </head>
+</html>
--- validateLogin.php
+++ validateLogin.php
@@ -18,11 +18,21 @@
     $sql = "SELECT Username, Password FROM Users WHERE Username = '$username' AND Password = '$password'";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
-    
+
+
+
 
     if($stmt->rowCount()>0){
-
+        
         $_SESSION["username"] = $username;
+
+        $sql = "SELECT `Admin` FROM `Users` WHERE`Username`= 'Max2'";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        $admin = $data['Admin'];
+        $_SESSION["admin"] = $admin;
+
         header("Location: homepage.php");
         exit();
 
