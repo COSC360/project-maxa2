2f919c7ea0cd524576b96f421416d78243cc3125
maxa2 <maxa@live.ca>
2023-04-07 15:43:10 -0700

    Update phpTest.php

--- phpTest.php
+++ phpTest.php
@@ -2,7 +2,7 @@
     use PHPUnit\Framework\TestCase;
 
     //Test checks if page loads properly
-    class Test extends \PHPUnit_Framework_TestCase{
+    class Test extends PHPUnit_Framework_TestCase{
         public function test(){
             $content = file_get_contents("homepage.php");
             $this->assertNotEmpty($content);
